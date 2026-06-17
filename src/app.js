99aa7673c6e7119bb7f8b63ba8ddad8333b75f4b
Websheet <websheetweb@gmail.com>
2026-06-17 19:44:31 +0700

    penambahan fitur blog

--- src/app.js
+++ src/app.js
@@ -29,8 +29,11 @@ connectDB();
 
 const app = express();
 app.use(expressLayouts);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+ 
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+
+
 const server = http.createServer(app);
 
 app.set("view engine", "ejs");
